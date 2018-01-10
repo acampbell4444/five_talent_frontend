06a50c95bba3b34bb53ea2d52e1128d781bd04b0
Alan Campbell <alancampbell4444@gmail.com>
2018-01-10 14:36:58 -0800

    s

--- src/components/House.js
+++ src/components/House.js
@@ -3,33 +3,33 @@ import React from 'react'
 export default function House({idx, house, editHouse, deleteHouse, history}) {
   return (
       <div className='well' key={idx}>
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <p>MLS # {house.mls_num}</p>
-                    <p>Street 1: {house.street1}</p>
-                    <p>Street 2: {house.street2}</p>
-                    <p>City: {house.city}</p>
-                    <p>State: {house.stateName}</p>
-                    <p>ZipCode: {house.zipCode}</p>
-                    <p>Neighborhood: {house.neighborhood}</p>
-                    <p>Sales Price: ${house.sales_price}</p>
-                    <p>Date Listed: {house.date_listed}</p>
-                    <p>Bedrooms: {house.bedrooms}</p>
-                    <p>Bathrooms: {house.bathrooms}</p>
-                    <p>Photo: {house.photo}</p>
-                    <p>SquareFeet: {house.squareFeet}</p>
-                    <p>Description: {house.description}</p>
-                  </div>
-                  <div className='col-sm-3'>
-                    {house.photo&&(<img src={house.photo} className='img-thumbnail' />)}
-                    {!house.photo&&(<img src="https://i.pinimg.com/736x/47/b9/7e/47b97e62ef6f28ea4ae2861e01def86c--my-dream-house-dream-big.jpg" className='img-thumbnail'/>)}
-                  </div>
-                  <div className='col-sm-3'>
-                    <button className='btn btn-primary' onClick={e=>{e.preventDefault();  editHouse(house._id); history.push('/edit-house');}}>Edit</button>
-                    <button className='btn btn-danger' onClick={e=>{e.preventDefault(); deleteHouse(house._id)}}>Delete</button>
-                  </div>
-                </div>
-              </div>
-    
+        <div className='row'>
+          <div className='col-sm-6'>
+            <p>MLS # {house.mls_num}</p>
+            <p>Street 1: {house.street1}</p>
+            <p>Street 2: {house.street2}</p>
+            <p>City: {house.city}</p>
+            <p>State: {house.stateName}</p>
+            <p>ZipCode: {house.zipCode}</p>
+            <p>Neighborhood: {house.neighborhood}</p>
+            <p>Sales Price: ${house.sales_price}</p>
+            <p>Date Listed: {house.date_listed}</p>
+            <p>Bedrooms: {house.bedrooms}</p>
+            <p>Bathrooms: {house.bathrooms}</p>
+            <p>Photo: {house.photo}</p>
+            <p>SquareFeet: {house.squareFeet}</p>
+            <p>Description: {house.description}</p>
+          </div>
+          <div className='col-sm-3'>
+            {house.photo&&(<img src={house.photo} className='img-thumbnail' />)}
+            {!house.photo&&(<img src="https://i.pinimg.com/736x/47/b9/7e/47b97e62ef6f28ea4ae2861e01def86c--my-dream-house-dream-big.jpg" className='img-thumbnail'/>)}
+          </div>
+          <div className='col-sm-3'>
+            <button className='btn btn-primary' onClick={e=>{e.preventDefault();  editHouse(house._id); history.push('/edit-house');}}>Edit</button>
+            <button className='btn btn-danger' onClick={e=>{e.preventDefault(); deleteHouse(house._id)}}>Delete</button>
+          </div>
+        </div>
+    </div>
+
   )
 }
--- src/components/IndexHouse.js
+++ src/components/IndexHouse.js
@@ -3,27 +3,18 @@ import { Link } from 'react-router'
 import House  from './House'
 import SearchBar  from './SearchBar'
 
-class IndexHouse extends Component {
+export default function IndexHouse({ history, allHouses, deleteHouse, editHouse, updateSearchFilter, selectedFilter, handleChange, searchTerm, filteredHouses }) {
+  const filterList = ['mls_num', 'date_listed', 'city', 'state', 'zipCode', 'bedrooms', 'bathrooms', 'squareFeet', 'no filter']
+  const houses = filteredHouses ? filteredHouses : allHouses
+  return (
+    <div>
+      <SearchBar filterList={filterList} updateSearchFilter={updateSearchFilter} selectedFilter={selectedFilter} handleChange={handleChange} searchTerm={searchTerm}/>
+      {
+        houses.map((house,i)=>
+          <House key={i} idx={i} house={house} editHouse={editHouse} deleteHouse={deleteHouse} history={history} />
+        )
+      }
+    </div>
+  )
+}
 
-  constructor(props) {
-      super(props)
-      this.state = {}
-    }
-    render() {
-      const { history, allHouses, deleteHouse, editHouse, updateSearchFilter, selectedFilter, handleChange, searchTerm, filteredHouses } = this.props
-      const filterList = ['mls_num', 'date_listed', 'city', 'state', 'zipCode', 'bedrooms', 'bathrooms', 'squareFeet', 'no filter']
-      let houses = filteredHouses ? filteredHouses : allHouses
-      return (
-        <div>
-          <SearchBar filterList={filterList} updateSearchFilter={updateSearchFilter} selectedFilter={selectedFilter} handleChange={handleChange} searchTerm={searchTerm}/>
-          {
-            houses.map((house,i)=>
-              <House key={i} idx={i} house={house} editHouse={editHouse} deleteHouse={deleteHouse} history={history} />
-            )
-          }
-        </div>
-      )
-    }
-  }
-
-export default IndexHouse;
