3cbe38508f5104a296cf8bd9ed75b52ba15d8928
Alan Campbell <alancampbell4444@gmail.com>
2018-01-10 14:18:43 -0800

    s

--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
-
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
-
-//import {whoami} from './reducers/auth'
 
 const store = createStore(
   rootReducer,
